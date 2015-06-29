61df9206ed554d14f33b20f3cedb11ba8481d3ed
Garett Dunn <garettmd@gmail.com>
2015-06-28 21:48:05 -0500

    Final final touches and readme

---
+++ readme.md
@@ -0,0 +1,12 @@
+#Puppet Install
+
+###Steps
+- Installs pre-req packages (git, perl, java)
+- Cleans up old install directory
+- Creates stash user
+- Creates stash home directory
+- Creates stash install directory
+- Downloads .tar file from Atlassian
+- Extracts tar file
+- Moves server.xml
+- Starts stash
--- site.pp
+++ site.pp
@@ -24,7 +24,7 @@ class stash {
 		user => "stash",
 		require => [Package["default-jre"], 
 					Package["git"], 
-					Pacakge["perl"],
+					Package["perl"],
 					Exec["get_stash_file"], 
 					Tidy["clean_old_install"]],
 	}
@@ -38,12 +38,15 @@ class stash {
 
 	file { "/home/stash":
 		ensure => directory,
+		owner => "stash",
+		group => "users",
+		mode => "0644",
+		require => User["stash"],
 	}
 
 	user { "stash":
 		name => "stash",
 		home => $stash_home,
-		require => File["/home/stash"],
 	}
 
 	exec { "get_stash_file":
@@ -61,7 +64,15 @@ class stash {
 		user => "stash",
 		path => $path_dir,
 		cwd => "$stash_install/atlassian-stash-3.10.2/bin",
+		require => Exec["move_server_xml"],
+	}
+
+	exec { "move_server_xml":
+		command => "cp -p $stash_install/atlassian-stash-3.10.2/conf/server.xml $stash_home/server.xml",
 		require => Exec["stash_install"],
+		path => $path_dir,
+		user => "stash",
+		creates => "$stash_home/server.xml",
 	}
 
 	tidy { "clean_old_install": 
