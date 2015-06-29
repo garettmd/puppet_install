d31e85247991f92ea966886d04f037f1a8917bd4
Garett Dunn <garettmd@gmail.com>
2015-06-28 21:17:58 -0500

    Added perl dependency; cleaned up requirements; fixed order

--- site.pp
+++ site.pp
@@ -1,6 +1,6 @@
 $stash_home = "/home/stash"
 $stash_install = "/stash"
-$path = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+$path_dir = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 include stash
 
@@ -12,43 +12,59 @@ package { "git":
 	ensure => present,
 }
 
+package { "perl":
+	ensure => present,
+}
+
 class stash {
 	exec { "stash_install":
-		command => "tar zx /tmp/atlassian-stash-3.10.2.tar.gz",
-		path => $path,
+		command => "tar zxf /tmp/atlassian-stash-3.10.2.tar.gz",
+		path => $path_dir,
 		cwd => $stash_install,
 		user => "stash",
-		onlyif => [ "ls /tmp/atlassian-stash-3.10.2.tar.gz",
-					"dpkg -s git",
-					"dpkg -s default-jre",],
+		require => [Package["default-jre"], 
+					Package["git"], 
+					Exec["get_stash_file"], 
+					Tidy["clean_old_install"]],
 	}
 
 	file { "/stash":
 		ensure => directory,
 		owner => "stash",
 		mode => "0660",
-		require => User["stash"],
+		require => [User["stash"], Tidy["clean_old_install"]],
+	}
+
+	file { "/home/stash":
+		ensure => directory,
 	}
 
 	user { "stash":
 		name => "stash",
 		home => $stash_home,
+		require => File["/home/stash"],
 	}
 
 	exec { "get_stash_file":
 		command => "wget https://www.atlassian.com/software/stash/downloads/binary/atlassian-stash-3.10.2.tar.gz",
 		cwd => "/tmp",
-		path => $path,
+		path => $path_dir,
 		user => "stash",
 		require => User["stash"],
 		creates => "/tmp/atlassian-stash-3.10.2.tar.gz",
 	}
 
 	exec { "start_stash":
-		command => "$stash_install/atlassian-stash-3.10.2/bin/start_stash.sh",
+		command => "$stash_install/atlassian-stash-3.10.2/bin/start-stash.sh",
 		environment => "STASH_HOME=$stash_home",
 		user => "stash",
-		path => $path,
+		path => $path_dir,
 		cwd => "$stash_install/atlassian-stash-3.10.2/bin",
+		require => Exec["stash_install"],
 	}
+
+	tidy { "clean_old_install": 
+		path => "$stash_install",
+		recurse => true,
+}
 }
